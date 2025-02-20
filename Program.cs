8c7d12a21782b6fa535820cf841d90ace2e53944
CapnSalty <austinrc53@hotmail.com>
2025-02-20 17:58:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace alphabeticalName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of students: ")
+            string[] studentNames = new string[int.Parse(Console.ReadLine())];
+            
+
+            Console.WriteLine("\nEnter in the name of the students: ");
+
+            for (int i = 0; i < studentNames.Length; i++)
+            {
+                studentNames[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nHere they are aphabetically: ");
+
+            Array.Sort(studentNames);
+
+            for (int i = 0; i < studentNames.Length; i++)
+            {
+                Console.WriteLine(studentNames[i]);
+            }
+
+            //Wait before closing
+            Console.ReadKey();
+        }
+    }
+}
